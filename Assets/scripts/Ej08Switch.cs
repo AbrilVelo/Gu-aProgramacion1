5c68f249fbbc1007873b3cd5eb2a2af50abaa4e9
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-03-31 14:19:52 -0300

    Ejercicio8 Switch
    Ejercicio 8 pero cambiado de IF a Switch

---
+++ Assets/scripts/Ej08Switch.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej08Switch : MonoBehaviour
+{
+    public int dia;
+    void Start()
+    {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miercoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            case 6:
+                Debug.Log("Viernes");
+                break;
+            case 7:
+                Debug.Log("Sabado");
+                break;
+            default:
+                Debug.Log("Día ingresado no es válido");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
